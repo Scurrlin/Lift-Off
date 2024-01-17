d55d00034799d91e75b950c3e935e552d07f4cec
Sean Currlin <sean.currlin@gmail.com>
2024-01-17 12:25:10 -0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
+
+  const [name, setName] = useState('')
+  const [weight, setWeight] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!user) {
+      setError('You must be logged in to add a workout')
+      return
+    }
+
+    const workout = {name, weight, reps}
+    
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.error)
+      setEmptyFields(json.emptyFields)
+    }
+    if (response.ok) {
+      setName('')
+      setWeight('')
+      setReps('')
+      setError(null)
+      setEmptyFields([])
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Workout</h3>
+
+      <label>Excersize Name:</label>
+      <input 
+        type="text" 
+        onChange={(e) => setName(e.target.value)} 
+        value={name}
+        className={emptyFields.includes('name') ? 'error' : ''}
+      />
+
+      <label>Weight (in lb.):</label>
+      <input 
+        type="number" 
+        onChange={(e) => setWeight(e.target.value)} 
+        value={weight}
+        className={emptyFields.includes('weight') ? 'error' : ''}
+      />
+
+      <label>Number of Reps:</label>
+      <input 
+        type="number" 
+        onChange={(e) => setReps(e.target.value)} 
+        value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
+      />
+
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  )
+}
+
+export default WorkoutForm
