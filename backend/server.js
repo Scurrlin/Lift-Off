f214c886ce4280218a63fb8ae6ef2502c173d63a
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 01:01:08 -0500

    server url fix

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use('/api/user', userRoutes)
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://lift-off.fun');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
