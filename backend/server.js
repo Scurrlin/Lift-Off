7892dc10bdba78fe4819e8b2fb15a0acc75d1c19
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 00:52:23 -0500

    final build update 2

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://www.lift-off.fun');
+  res.header('Access-Control-Allow-Origin', 'https://lift-off.fun');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('https://lift-off.onrender.com//api/workouts', {
+      const response = await fetch('https://lift-off.onrender.com/api/workouts', {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
