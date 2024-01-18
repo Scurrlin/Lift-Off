d701eaf2602f69441b7319ffe13c7a1dc268964a
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 00:24:01 -0500

    update login/sign up and logout

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,16 +1,16 @@
-import { useAuthContext } from './useAuthContext'
-import { useWorkoutsContext } from './useWorkoutsContext'
+import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 export const useLogout = () => {
-  const { dispatch } = useAuthContext()
-  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
+  const { dispatch: dispatchAuth } = useAuthContext();
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext();
 
   const logout = () => {
-    localStorage.removeItem('user')
+    localStorage.removeItem('user');
 
-    dispatch({ type: 'LOGOUT' })
-    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
-  }
+    dispatchAuth({ type: 'LOGOUT' });
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null });
+  };
 
-  return { logout }
-}
+  return { logout };
+};
