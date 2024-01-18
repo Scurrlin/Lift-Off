479212002764b96b735fa27c1e9974a8d4eeae28
Sean Currlin <sean.currlin@gmail.com>
2024-01-18 01:19:31 -0500

    reinstall cors dependency

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,31 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://lift-off.fun');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  console.log(req.path, req.method);
   next();
 });
+
+app.use(cors());
+
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
